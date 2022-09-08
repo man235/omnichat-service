d06db0f32cca2706a2636cb721306514dffc3cba
Tai <lemanhtaictbp@gmail.com>
2022-09-08 10:35:14 +0700

    feat: remove api list room

--- sop_chat_service/app_connect/api/room_views.py
+++ sop_chat_service/app_connect/api/room_views.py
@@ -98,29 +98,7 @@ class RoomViewSet(viewsets.ModelViewSet):
         room.completed_date = datetime.now(tz=timezone.utc)
         room.save()
         return Response(status=status.HTTP_200_OK)
-    def list(self, request, *args, **kwargs):
-        qs = Room.objects.filter(room_message__is_sender=False, completed_date__isnull=True).order_by(
-        '-created_at')
-        if 'type' in request.query_params:
-            type = request.query_params['type']
-            qs = qs.filter(type=request.query_params['type'])
-            if 'status' in request.query_params:
-                filter = request.query_params['status']
-                if filter.lower() == 'waiting':
-                    qs = qs.filter(type=request.query_params['type'], approved_date__isnull=True)
-                elif filter.lower() == 'processing':
-                    qs = qs.filter(type=request.query_params['type'],
-                                   approved_date__isnull=False, completed_date__isnull=True)
-        elif 'status' in request.query_params:
-            filter = request.query_params['status']
-            if filter.lower() == 'waiting':
-                qs = qs.filter(approved_date__isnull=True)
-            elif filter.lower() == 'processing':
-                qs = qs.filter(approved_date__isnull=False, completed_date__isnull=True)
-        elif 'name' in request.query_params:
-            qs = qs.filter(name__icontains=request.query_params['name'])
-        sz = RoomMessageSerializer(qs, many=True)
-        return custom_response(200, "Get List Room Successfully", sz.data)
+   
     def retrieve(self, request, pk=None):
         room = Room.objects.filter(id =pk).first()        
         if room:
