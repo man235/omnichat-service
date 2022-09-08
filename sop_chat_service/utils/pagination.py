5db95bc6d7581747f5052b07073911cadf1dafc5
Tai <lemanhtaictbp@gmail.com>
2022-09-08 11:47:38 +0700

    feat: api get message room and fix time zone

---
+++ sop_chat_service/utils/pagination.py
@@ -0,0 +1,21 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class Pagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 30
+
+    def get_paginated_response(self, data):
+        custom = {
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'page_size': self.page_size,
+            'data': data
+        }
+        return custom
