a526c24353729d6ec33435b488cbbfc8e5efc932
kimthien2603 <nguyenkimthien2603@gmail.com>
2022-09-13 17:16:56 +0700

    Fix: fix subscribe topic on nats sop

--- core/nats/topic_message_facebook.py
+++ core/nats/topic_message_facebook.py
@@ -12,31 +12,33 @@ logger = logging.getLogger(__name__)
 
 async def subscribe_channels(topics):
     await nats_client.connect(servers=[settings.NATS_URL])
-    all_fanpage = FanPage.objects.all()
-    for fanpage in all_fanpage:
-        topic = topics+fanpage.page_id
-        logger.debug(f'Before Subscribe natsUrl --------------------------------------------------- {topic} -------- {nats_client.is_connected}')
+    # all_fanpage = FanPage.objects.all()
+    # for fanpage in all_fanpage:
+    #     topic = topics+fanpage.page_id
+    #     logger.debug(f'Before Subscribe natsUrl --------------------------------------------------- {topic} -------- {nats_client.is_connected}')
         
-        async def subscribe_handler(msg):
-            try:
-                logger.debug(f'data subscribe natsUrl ----------------- {msg.data}')
-                data = json.loads((msg.data.decode("utf-8")).replace("'", "\""))
-                room = await check_room_facebook(data)
-                if not room:
-                    return      # No Fanpage to subscribe
-                new_topic_publish = f'message_{room.room_id}'
-                await nats_client.publish(new_topic_publish, bytes(msg.data))
-                await save_message_store_database(room, data)
-            except Exception as e:
-                logger.debug(f'Exception subscribe ----------------- {e}')
+    async def subscribe_handler(msg):
+        try:
+            logger.debug(f'data subscribe natsUrl ----------------- {msg.data}')
+            data = json.loads((msg.data.decode("utf-8")).replace("'", "\""))
+            room = await check_room_facebook(data)
+            if not room:
+                return      # No Fanpage to subscribe
+            new_topic_publish = f'message_{room.room_id}'
+            await nats_client.publish(new_topic_publish, bytes(msg.data))
+            await save_message_store_database(room, data)
+        except Exception as e:
+            logger.debug(f'Exception subscribe ----------------- {e}')
 
-        await nats_client.subscribe(topic, "worker", cb=subscribe_handler)
-        logger.debug(f'After Subscribe natsUrl --------------------------------------------------- ')
+        # await nats_client.subscribe(topics, "worker", cb=subscribe_handler)
+    for topic in topics:
+        await nats_client.subscribe(topic, "worker", subscribe_handler)
+    logger.debug(f'After Subscribe natsUrl --------------------------------------------------- ')
 
 async def main():
     logger.debug(f'data subscribe natsUrl ----------------- {nats_client} -------- ')
-    # topics = settings.CHANNELS_SUBSCRIBE
-    topics = "omniChat.message.receive."
+    topics = settings.CHANNELS_SUBSCRIBE
+    # topics = "omniChat.message.receive.*"
     await asyncio.gather(
         subscribe_channels(topics),
     )
