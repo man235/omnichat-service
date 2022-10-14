316d8e00924a90742e8e7bc3d4e03409733cbf44
COPbox269 <tranngocman210398@gmail.com>
2022-10-14 10:06:22 +0700

    save recieved message to minio

--- sop_chat_service/zalo/utils/chat_support/save_message_zalo.py
+++ sop_chat_service/zalo/utils/chat_support/save_message_zalo.py
@@ -2,9 +2,11 @@ from core.schema.message_receive import ChatOptional
 from sop_chat_service.app_connect.models import Message, Attachment, Room
 from django.utils import timezone
 from core.schema import MessageWebSocket
+from django.conf import settings
 import uuid
 
 from sop_chat_service.zalo.utils.api_suport.api_zalo_caller import get_message_data_of_zalo_user
+from sop_chat_service.zalo.utils.chat_support.upload_file_to_minio import upload_file_to_minio_zalo
 
 
 async def save_message_store_database_zalo(
@@ -26,22 +28,32 @@ async def save_message_store_database_zalo(
     message.save()
     if msg.attachments:
         for index, attachment in enumerate(msg.attachments):
-            if not optionals[index] and not optionals[index].data.get('attachments'):
-                attachment_name = None
-                attachment_size = None
-                pass
-            else:
-                optional_attachment_payload = optionals[index].data.get('attachments')[index].get('payload')
-                attachment_name = optional_attachment_payload.get('name')
-                attachment_size = optional_attachment_payload.get('size')
+            domain = settings.DOMAIN_MINIO_SAVE_ATTACHMENT
+            sub_url = f"api/live_chat/chat_media/get_chat_media?name=live_chat_room_{room.room_id}/"
+            print(f'url +++++++++++++++++++++++++++++++++')
+            url = ''.join([domain, sub_url])
+            print(f'url +++++++++++++++++++++++++++++++++ {url}')
+            data_upload_file = upload_file_to_minio_zalo(attachment, room.room_id)
+            print(f'store_sending_message_zalo ---------- data_upload_file-{data_upload_file}')  
+            
+            # if not optionals[index] and not optionals[index].data.get('attachments'):
+            #     attachment_payload_name = None
+            #     attachment_payload_size = None
+            #     attachment_payload_type = None
+            #     pass
+            # else:
+            #     optional_attachment_payload = optionals[index].data.get('attachments')[index].get('payload')
+            #     attachment_payload_name = optional_attachment_payload.get('name')
+            #     attachment_payload_size = optional_attachment_payload.get('size')
+            #     attachment_payload_type = optional_attachment_payload.get('type')
             
             Attachment.objects.create(
                 mid = message,
-                type = attachment.type,
-                attachment_id = optional_attachment_payload.get('id'),
-                url = attachment.url,
-                name = attachment_name,
-                size = attachment_size
+                type = '/'.join([attachment.type, 'somethingelse']),
+                # attachment_id = optional_attachment_payload.get('id'),
+                url = ''.join([url, data_upload_file]),
+                # name = attachment_payload_name,
+                # size = attachment_payload_size
             )
 
 def fetch_attachment_url_zalo(
