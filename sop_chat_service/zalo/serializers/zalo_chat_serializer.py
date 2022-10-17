aab5ba5b75c7a7b04eba4ac223b2ba79ab150727
COPbox269 <tranngocman210398@gmail.com>
2022-10-17 08:52:12 +0700

    support minio to save attachment beta

--- sop_chat_service/zalo/serializers/zalo_chat_serializer.py
+++ sop_chat_service/zalo/serializers/zalo_chat_serializer.py
@@ -1,63 +1,44 @@
+from pkg_resources import require
 from rest_framework import serializers
 from sop_chat_service.app_connect.models import Attachment, Message
+from sop_chat_service.zalo.utils.chat_support.format_message_zalo import format_attachment_type
 from sop_chat_service.zalo.utils.chat_support.type_constant import *
 
 
-class ZaloChatSerializer(serializers.Serializer):
+class ZaloTextMessage(serializers.Serializer):
     room_id = serializers.CharField(required=True)
     recipient_id = serializers.CharField(required=True)
-    message_type = serializers.CharField(required=True)
-    text = serializers.CharField(required=False)
-    attachment_token = serializers.CharField(required=False)
-    attachment_id = serializers.CharField(required=False)
+    text = serializers.CharField(required=True)
     
-    def check_validation(self, data):
-        message_type_check = str(data.get('message_type')).lower()
-        if message_type_check == TEXT_MESSAGE:
-            if not data.get('text'):
-                raise serializers.ValidationError('Text is required')
-        elif message_type_check == FILE_MESSAGE:
-            if not data.get('attachment_token'):
-                raise serializers.ValidationError('Attachment token is required')
-        elif message_type_check in (STICKER_MESSAGE, IMAGE_MESSAGE, GIF_MESSAGE):
-            if not data.get('attachment_id'):
-                raise serializers.ValidationError('Attachment id is required')
-        else:
-            raise serializers.ValidationError(f'Invalid message type. No support for {message_type_check}')
-    
-    
-class ZaloUploadSerializer(serializers.Serializer):
-    attachment_type = serializers.CharField(required=True)
-    file = serializers.FileField(required=True)
+
+class ZaloAttachmentMessage(serializers.Serializer):
     room_id = serializers.CharField(required=True)
+    recipient_id = serializers.CharField(required=True)
+    type = serializers.CharField(required=True) # file -attachment_token (doc, pdf, csv), image -attachment_id
+    file = serializers.FileField(required=True)
     
-    def check_validation(self, request, data):
-        # Get content type
-        attachment_type_check = str(data.get('attachment_type')).lower()
-        file = request.FILES['file']       
+    def check_file(self, request, data):
+        attachment_type = data.get('type')
+        file = request.FILES['file']
         
-        file_content_type = str(file.content_type).lower().split('/')[0]           
-        
-        # file_content_type = files.content_type
-        if attachment_type_check == FILE_MESSAGE:
-            if not file_content_type == 'application':
+        try:
+            file_extension = str(file.name).split('.')[1]
+            file_type = '/'.join(file.content_type)
+            # print(f'file_type1 -------- {file.type}')
+            if attachment_type == FILE_MESSAGE:
+                if file_extension in FILE_DOC_EXTENSION:
+                    file_type = '/'.join([FILE_CONTENT_TYPE, FILE_MSWORD_EXTENSION])
+                else:
+                    file_type = '/'.join([FILE_CONTENT_TYPE, file_extension])
+            elif attachment_type == IMAGE_MESSAGE:
+                file_type = '/'.join([attachment_type, file_extension])
+            else:
                 raise serializers.ValidationError({
-                    'message': 'Only support for doc, pdf, csv'
+                    'message': f'Invalid type. No support for {attachment_type}'
                 })
-        elif attachment_type_check == IMAGE_MESSAGE:
-            if not file_content_type == 'image':
-                raise serializers.ValidationError({
-                    'message': 'Only support for png, jpeg'
-                })
-        elif attachment_type_check == GIF_MESSAGE:
-            if not file_content_type == 'image':
-                raise serializers.ValidationError({
-                    'message': 'Only support for gif'
-                })
-        else:
+        except Exception as e:
             raise serializers.ValidationError({
-                'message': f'Invalid file. No support for {attachment_type_check}'
+                'message': 'Can not format file type'
             })
-        
-        return file
-        
+                    
+        return file, file_type
