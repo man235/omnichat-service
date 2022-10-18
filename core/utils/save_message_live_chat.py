c46e22ba2949e39d3c6be81b38073284bdbf0ec5
kimthien2603 <nguyenkimthien2603@gmail.com>
2022-10-18 17:17:48 +0700

    fix: storage message and check room live chat

--- core/utils/save_message_live_chat.py
+++ core/utils/save_message_live_chat.py
@@ -1,6 +1,6 @@
 from core.schema.message_receive import NatsChatMessage
 from sop_chat_service.app_connect.models import Message, Attachment, Room, ServiceSurvey
-from sop_chat_service.live_chat.models import LiveChat
+# from sop_chat_service.live_chat.models import LiveChat
 
 
 
@@ -8,8 +8,8 @@ from sop_chat_service.live_chat.models import LiveChat
 async def live_chat_save_message_store_database(room, data: NatsChatMessage):
 
     if room.type =="livechat":
-        live_chat = LiveChat.objects.filter(id = data.recipientId).first()
-        room = Room.objects.filter(room_id = room).first().update(user_id = live_chat.user_id)
+        # live_chat = LiveChat.objects.filter(id = data.recipientId).first()
+        # room = Room.objects.filter(room_id = room).first().update(user_id = live_chat.user_id)
         message = Message(
             room_id = room,
             fb_message_id = data.mid,
