affa9a3e6db2080e82e878893333dcefa923c26e
kimthien2603 <nguyenkimthien2603@gmail.com>
2022-10-31 18:20:12 +0700

    fix: issue 802, 798, 794, 792, 778

--- core/storage_handler/store_database.py
+++ core/storage_handler/store_database.py
@@ -3,9 +3,9 @@ from core.schema import CoreChatInputMessage
 from core import constants
 from core.handlers import BaseHandler
 from core.schema.message_receive import ChatOptional, NatsChatMessage
-from core.utils import format_receive_message, format_receive_message_zalo, facebook_save_message_store_database
+from core.utils import format_receive_message, facebook_save_message_store_database
 from core.utils.save_message_live_chat import live_chat_save_message_store_database
-from sop_chat_service.zalo.utils import save_message_store_database_zalo, save_msg_store_database_zalo
+from sop_chat_service.zalo.utils import save_message_store_database_zalo
 
 
 class StorageDataBase(BaseHandler):
@@ -15,15 +15,10 @@ class StorageDataBase(BaseHandler):
         message_storage = format_receive_message(room, data)
         
         if data.typeChat == 'facebook':
-            # await facebook_save_message_store_databases(room, message_storage)
             await facebook_save_message_store_database(room, data)
         elif data.typeChat == 'zalo':
             optionals: list[ChatOptional] = data.optionals
             await save_message_store_database_zalo(room, message_storage, optionals)
-            # msg_storages = format_receive_message_zalo(room, data)
-            # for msg_storage in msg_storages:
-                # await save_message_store_database_zalo(room, msg_storage, optionals)
-            # await save_msg_store_database_zalo(room, msg_storages[0], optionals)
         elif data.typeChat == "livechat":
             await live_chat_save_message_store_database(room,data)
         
