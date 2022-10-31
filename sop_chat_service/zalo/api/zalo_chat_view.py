0bb0bcb360dd1d67371896268b9b4f59f17b3907
kimthien2603 <nguyenkimthien2603@gmail.com>
2022-10-31 10:55:20 +0700

    fix: room block for FE

--- sop_chat_service/zalo/api/zalo_chat_view.py
+++ sop_chat_service/zalo/api/zalo_chat_view.py
@@ -85,11 +85,11 @@ class ZaloChatViewSet(viewsets.ModelViewSet):
                 )
             if not queryset.block_admin:
                 if queryset.admin_room_id == user_header:
-                    queryset.user_id == user_header
+                    queryset.user_id = user_header
                     queryset.admin_room_id = None
                     queryset.save()
                 elif queryset.user_id == user_header:
-                    queryset.user_id == user_header
+                    queryset.user_id = user_header
                     queryset.block_admin = True
                     queryset.save()
             message_data_to_socket = format_sended_message_to_socket(
