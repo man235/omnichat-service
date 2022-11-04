ca242ad21243f8a88622b22a9b5637221a386409
man235 <tranngocman210398@gmail.com>
2022-11-04 09:08:04 +0700

    fix define log message constant

---
+++ core/constants/log_messages.py
@@ -0,0 +1,14 @@
+MESSAGE_TYPE_LOG = 'log'
+
+TRIGGER_COMPLETED = 'COMPLETED'
+TRIGGER_REOPENED = 'REOPENED'
+TRIGGER_REMINDED = 'REMINDED'
+TRIGGER_LEFT = 'LEFT'
+TRIGGER_FORWARDED = 'FORWARDED'
+
+# LOG MESSAGE SAMPLES
+LOG_COMPLETED = '{from_user} have completed the conversation'
+LOG_REOPENED = '{from_user} have reopened the conversation'
+LOG_REMINDED = '{from_user} has set up remider "{remider}"'
+LOG_LEFT = '{from_user} have left the conversation'
+LOG_FORWARDED = '{from_user} has forwarded the message to {to_user}'
