765327a69e0e7c6096a2932126e4457d62f9c50f
kimthien2603 <nguyenkimthien2603@gmail.com>
2022-11-16 15:11:49 +0700

    fix: celery task verify customer

--- core/utils/format_data_celery.py
+++ core/utils/format_data_celery.py
@@ -18,9 +18,6 @@ async def celery_format_data_verify_customer(user_app: UserApp, room: Room):
         'fanpage': room.page_id.name,
         'fanpage_url': fanpage_url,
         'approach_date': str(room.created_at.isoformat()),
-        'ip': None,
-        'device': None,
-        'browser': None,
         "room_id": room.room_id
     }
     return data
