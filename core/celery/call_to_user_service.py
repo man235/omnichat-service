8aae4fb276b0fc3e6792ed457e77df998e1738ac
kimthien2603 <nguyenkimthien2603@gmail.com>
2022-11-16 16:11:26 +0700

    fix: verify customer information of zalo

--- core/celery/call_to_user_service.py
+++ core/celery/call_to_user_service.py
@@ -22,7 +22,7 @@ def celery_task_verify_information(_data: Dict, *args, **kwargs):
         url = settings.CUSTOMER_SERVICE_URL + settings.API_VERIFY_INFORMATION
         response = requests.post(url=url, headers=headers, data=_data)
         print(" *************************************************************************************** ", _data)
-        print(response.status_code, response.json())
+        print(response.status_code)
         return response.text
     except Exception as e:
         return f"Exception Verify information ERROR: {e}"
